153de67bdbc716faf5f044fa8f6c7b06609be331
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-12 14:41:58 -0700

    In ExperienceCard of WorkExperience component, scroll is added to make it scrollable and using flex to show horizontally. In ExperienceCard component, snap-center is added to change the background

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export default function ExperienceCard({}: Props) {
   return (
-    <article>
+    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10'>
       <motion.img
         initial={{
           y: -100,
@@ -19,15 +19,31 @@ export default function ExperienceCard({}: Props) {
         alt=''
       />
 
-      <div>
-        <h4>Software Engineer/ Full Stack Developer</h4>
-        <p>Freelance (Mission based)</p>
-        <div>
-          {/* Tech used */}
-          {/* Tech used */}
-          {/* Tech used */}
+      <div className='px-0 md:px-10'>
+        <h4 className='text-4xl font-light'>
+          Software Engineer/ Full Stack Developer
+        </h4>
+        <p className='font-bold text-2xl mt-1'>Freelance (Mission based)</p>
+        <div className='flex space-x-2 my-2'>
+          <img
+            className='h-10 w-10 rounded-full'
+            src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.-i781M2JNO6ZYT5VetHsSAHaDt%26pid%3DApi%26h%3D160&f=1&ipt=c80646d54b5ed4ce93bdbe155e782f9c1a38bc83ad0ec4b8689718297fda5c46&ipo=images'
+            alt=''
+          />
+          <img
+            className='h-10 w-10 rounded-full'
+            src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.-i781M2JNO6ZYT5VetHsSAHaDt%26pid%3DApi%26h%3D160&f=1&ipt=c80646d54b5ed4ce93bdbe155e782f9c1a38bc83ad0ec4b8689718297fda5c46&ipo=images'
+            alt=''
+          />
+          <img
+            className='h-10 w-10 rounded-full'
+            src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.-i781M2JNO6ZYT5VetHsSAHaDt%26pid%3DApi%26h%3D160&f=1&ipt=c80646d54b5ed4ce93bdbe155e782f9c1a38bc83ad0ec4b8689718297fda5c46&ipo=images'
+            alt=''
+          />
         </div>
-        <p>Started work... - Ended...</p>
+        <p className='uppercase py-5 text-gray-300'>
+          Started work... - Ended...
+        </p>
 
         <ul className='list-disc space-y-4 ml-5 text-lg'>
           <li>Summary points</li>
