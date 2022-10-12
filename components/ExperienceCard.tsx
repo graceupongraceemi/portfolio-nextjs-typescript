2d7846688e452f4c62f24c6bcb889f488cb89eef
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-12 14:51:27 -0700

    hover opacity is added to article div in ExperienceCard component

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export default function ExperienceCard({}: Props) {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10'>
+    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40'>
       <motion.img
         initial={{
           y: -100,
