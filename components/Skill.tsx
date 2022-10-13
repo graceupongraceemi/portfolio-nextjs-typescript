286bbacf6a5d52993a7441a93238ba69bf0813e9
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-13 08:59:42 -0700

    building the Skill component in the Skills section with images along with Framer motion

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -17,7 +17,14 @@ function Skill({ directionLeft }: Props) {
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
         src='https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png'
+        className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
       />
+
+      <div>
+        <div>
+          <p>100%</p>
+        </div>
+      </div>
     </div>
   );
 }
