c4f8ca88fbf5c1774d2db3f7673c2525199681d3
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-13 13:03:16 -0700

    The ContactMe component created and added to the index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import About from '../components/About';
+import ContactMe from '../components/ContactMe';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Projects from '../components/Projects';
@@ -36,7 +37,9 @@ const Home: NextPage = () => {
         <Projects />
       </section>
 
-      {/* Contact Me */}
+      <section id='contact' className='snap-start'>
+        <ContactMe />
+      </section>
     </div>
   );
 };
