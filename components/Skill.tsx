d5749e6cef4623c748fc1feb2e942eedbfa79ca4
Emiko Esther S <graceupongraceemi@gmail.com>
2022-10-12 19:29:00 -0700

    In Skill component, grou direction left items adde

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
-type Props = {};
+type Props = {
+  // Typescript
+  directionLeft?: boolean;
+};
 
-function Skill({}: Props) {
-  return <div>Skill</div>;
+function Skill({ directionLeft }: Props) {
+  return (
+    <div className='group relative flex cursor-pointer'>
+      <motion.img
+        initial={{
+          x: directionLeft ? -200 : 200,
+          opacity: 0
+        }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        src='https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png'
+      />
+    </div>
+  );
 }
 
 export default Skill;
